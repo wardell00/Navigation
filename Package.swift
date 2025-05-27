e54093af36d5a5b0ed346374d5671c880134fa03
Alexander Loewe <loewea2000@gmail.com>
2025-05-27 16:54:30 +0200

    Added minimum version support

--- Package.swift
+++ Package.swift
@@ -5,6 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "Navigation",
+    platforms: [.macOS(.v15), .iOS(.v18), .macCatalyst(.v18), .watchOS(.v10)],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
